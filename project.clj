d37dc55219ba0b023e55a953dff329a9af4854f3
Dmitri Sotnikov <dmitri.sotnikov@gmail.com>
2015-06-04 08:09:28 -0400

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject jdbc-ring-session "0.3"
+(defproject jdbc-ring-session "0.4"
   :description "Ring JDBC Session Store"
   :url "https://github.com/yogthos/jdbc-ring-session"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
  :dependencies [[org.clojure/clojure "1.6.0"]
                 [ring/ring-core "1.3.2"]
-                [com.taoensso/nippy "2.8.0"]
+                [com.taoensso/nippy "2.9.0"]
                 [commons-codec/commons-codec "1.10"]
                 [org.clojure/java.jdbc "0.3.6"]]
 
