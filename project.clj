47e67c1bed113b70c5dc208a6e550ebe72edffa9
Yogthos <dmitri.sotnikov@gmail.com>
2015-11-30 23:02:18 -0500

    bumped up version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject jdbc-ring-session "0.6"
+(defproject jdbc-ring-session "0.7"
   :description "Ring JDBC Session Store"
   :url "https://github.com/yogthos/jdbc-ring-session"
   :license {:name "Eclipse Public License"
